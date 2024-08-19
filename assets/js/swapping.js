bd099f73945be2488dfbba3a412485cf64dcdc70
Yousmakhanfullstackdeveloper <yousmakhan51@gmail,com>
2024-08-19 17:18:04 +0500

    done

---
+++ assets/js/swapping.js
@@ -0,0 +1,9 @@
+let a = 17;
+let b = 7;
+document.write("<h1>before swappig the variable a contain "+a+"</h1>")
+document.write("<h1>before swappig the variable a contain "+b+"</h1>")
+a=a+b;
+b=a-b;
+a=a-b;
+document.write("<h1>after swappig the variable a contain "+a+"</h1>")
+document.write("<h1>after swappig the variable a contain "+b+"</h1>")
